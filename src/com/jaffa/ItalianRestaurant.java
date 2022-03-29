cbcd89f4af25113be15013a37c1456ee2243d061
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2022-03-29 22:26:50 +0200

    Added some GUI design

--- src/com/jaffa/ItalianRestaurant.java
+++ src/com/jaffa/ItalianRestaurant.java
@@ -1,18 +1,62 @@
 package com.jaffa;
 
+import javax.swing.*;
 import com.jaffa.pizzaBuilder.CalzonePizzaBuilder;
 import com.jaffa.pizzaBuilder.Director;
 import com.jaffa.pizzaBuilder.pizzas.Calzone;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ItalianRestaurant
 {
     public static void main(String[] args)
     {
-        Director director = new Director();
-        CalzonePizzaBuilder builder = new CalzonePizzaBuilder();
-        director.constructCalzonePizza(builder);
+        JFrame landingPage = new JFrame();
+        landingPage.setSize(800, 800);
+        landingPage.setLayout(null);
+        landingPage.setVisible(true);
+        landingPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Calzone pizza = builder.getPizza();
-        System.out.println(pizza.printInfo());
+        JFrame orderPage = new JFrame();
+        orderPage.setSize(800, 800);
+        orderPage.setLayout(null);
+        orderPage.setVisible(false);
+        orderPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton cancel = new JButton("Cancel");
+        cancel.setBounds(0, 0, 100 ,40);
+        cancel.addActionListener(e ->
+        {
+            landingPage.setVisible(true);
+            orderPage.setVisible(false);
+        });
+
+        JButton next = new JButton("Next");
+        next.setBounds(100, 0, 100, 40);
+
+        JButton prev = new JButton("Previous");
+        prev.setBounds(200, 0, 100, 40);
+
+        JButton order = new JButton("Start Order");
+        order.setBounds(0, 0, 100, 40);
+        order.addActionListener(e ->
+        {
+            landingPage.setVisible(false);
+            orderPage.setVisible(true);
+        });
+
+        landingPage.add(order);
+
+        orderPage.add(cancel);
+        orderPage.add(next);
+        orderPage.add(prev);
+
+//        Director director = new Director();
+//        CalzonePizzaBuilder builder = new CalzonePizzaBuilder();
+//        director.constructCalzonePizza(builder);
+//
+//        Calzone pizza = builder.getPizza();
+//        System.out.println(pizza.printInfo());
     }
 }

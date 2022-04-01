7fc35e7e3f615247d47a4daf86fe5b2e648a2e15
dennisjhuu <dennisrozema1998@gmail.com>
2022-04-01 16:10:41 +0200

    added comments

--- src/com/jaffa/orderObserver/BleeperListener.java
+++ src/com/jaffa/orderObserver/BleeperListener.java
@@ -1,5 +1,8 @@
 package com.jaffa.orderObserver;
 
+/*
+* BleeperListener implements the EventListener interface and executes it's own functionality in the update method
+*/
 public class BleeperListener implements EventListener
 {
     private String orderMessage = "BLEEP BLEEP";
